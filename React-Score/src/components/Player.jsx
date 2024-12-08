434e5b83e7cdcc75a547b90c91d75ae0de2efa6c
owm30911 <owm30911@gmail.com>
2024-12-08 20:49:30 +0800

    useState convert useRef

--- React-Score/src/components/Player.jsx
+++ React-Score/src/components/Player.jsx
@@ -1,10 +1,31 @@
+import { useRef } from 'react';
+import { useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState(null);
+  // const [isSubmit, setIsSubmit] = useState(false);
+
+  // function handlePlayerName(event) {
+  //   setIsSubmit(false);
+  //   setPlayerName(event.target.value);
+  // }
+
+  const playerNameInput = useRef();
+
+  function handleClickPlayerName() {
+    setPlayerName(playerNameInput.current.value);
+    // setIsSubmit(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* <h2>Welcome {isSubmit ? playerName : 'unknown entity'}</h2> */}
+      {/* 可修改成以下 */}
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameInput} type="text" />
+        {/* <input type="text" value={playerName} onChange={handlePlayerName} /> */}
+        <button onClick={handleClickPlayerName}>Set Name</button>
       </p>
     </section>
   );
