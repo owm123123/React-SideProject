3be2995c4d6f907d485b0b1e7604e25762af6631
owm30911 <owm30911@gmail.com>
2024-12-11 18:51:12 +0800

    add dialog score & timeRemaining tool

--- React-Score/src/components/ResultModal.jsx
+++ React-Score/src/components/ResultModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, timeRemaining, onReset },
   ref
 ) {
   const dialogRef = useRef();
@@ -14,17 +14,23 @@ const ResultModal = forwardRef(function ResultModal(
     },
   }));
 
+  console.log(timeRemaining);
+  const userLost = timeRemaining <= 0;
+  const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2);
+  const score = (1 - timeRemaining / (targetTime * 1000)) * 100;
+
   return (
-    <dialog ref={dialogRef} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialogRef} className="result-modal" onClose={onReset}>
+      {userLost ? <h2>You lose</h2> : <h2>Your Score: {score}</h2>}
       <p>
         The Target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedTimeRemaining} seconds left.</strong>
       </p>
       <form method="dialog">
-        <button>Close</button>
+        <button onClick={onReset}>Close</button>
       </form>
     </dialog>
   );
