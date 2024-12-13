f85997c3fc90022a558b362fafd39a25249baacf
long hao <owm30912@mitac.com.tw>
2024-12-13 19:44:40 +0800

    learn useConext handle prop drilling

--- React-Cart/src/App.jsx
+++ React-Cart/src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
+import { CartContext } from './store/cart-items-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -65,14 +67,27 @@ function App() {
     });
   }
 
+  const cartCtxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <>
+    <CartContext.Provider value={cartCtxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop>
+        <ul id="products">
+          {DUMMY_PRODUCTS.map((product) => (
+            <li key={product.id}>
+              <Product {...product} onAddToCart={handleAddItemToCart} />
+            </li>
+          ))}
+        </ul>
+      </Shop>
+    </CartContext.Provider>
   );
 }
 
--- React-Cart/src/components/Cart.jsx
+++ React-Cart/src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from 'react';
+import { CartContext } from '../store/cart-items-context';
+
+export default function Cart({ onUpdateItemQuantity }) {
+  // 可以直接解構
+  const { items } = useContext(CartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
