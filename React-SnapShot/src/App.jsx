390aa3139556e2fc816666d7b22baecbff2670f9
owm30911 <owm30911@gmail.com>
2024-12-15 03:18:12 +0800

    learn useEffect navigator

--- React-SnapShot/src/App.jsx
+++ React-SnapShot/src/App.jsx
@@ -1,16 +1,39 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces') || []);
+// 當兩個 arr要比較時, 可以將一個 arr轉成 map, 再用arr.map(() => map.get())
+const placesMap = new Map(AVAILABLE_PLACES.map((place) => [place.id, place]));
+const storepickedPlaces = storedIds.map((id) => placesMap.get(id));
+
+// const storepickedPlaces = AVAILABLE_PLACES.filter(
+//   (place) => storedIds.indexOf(place.id) !== -1
+// );
+
+// const storepickedPlaces = storedIds.map((id) =>
+//   AVAILABLE_PLACES.find((price) => price.id === id)
+// );
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storepickedPlaces);
+
+  // unnecessary use Effect
+  // useEffect(() => {
+  //   const storedIds = JSON.parse(localStorage.getItem('selectedPlaces') || []);
+  //   console.log(storedIds);
+  //   setPickedPlaces(
+  //     AVAILABLE_PLACES.filter((place) => storedIds.indexOf(place.id) !== -1)
+  //   );
+  // }, []);
 
   // navigator: brower info (類似 window)
   // user info: navigator.userAgent
@@ -21,15 +44,21 @@ function App() {
   // successCallback: (position) => {} (並填) excute when success
   // errorCallback: (error) => {} (選填) execute when error
   // option: () => {} (選填) get position method
-  navigator.geolocation.getCurrentPosition((position) => {
-    const sortedPlaces = sortPlacesByDistance(
-      AVAILABLE_PLACES,
-      position.coords.latitude,
-      position.coords.longitude
-    );
 
-    setAvailablePlaces(sortedPlaces);
-  });
+  // useEffect(() => {}, []) handle other business (ex: localStorage/call api ...)
+  // [] (empty arr): execute function when init render
+  // [state1, state1]: execute function when specific state
+  // none (not arr): execute function when all state
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -48,6 +77,15 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1) {
+      console.log('add store success');
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -55,6 +93,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id != selectedPlace.current))
+    );
   }
 
   return (
