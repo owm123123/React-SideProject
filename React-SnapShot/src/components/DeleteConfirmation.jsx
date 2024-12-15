c0dbe089990244b37b1a7e85a72a130da2353a06
owm30911 <owm30911@gmail.com>
2024-12-15 14:24:46 +0800

    learn useEffect setTimeout dialog & useEffect return ()=>{}

--- React-SnapShot/src/components/DeleteConfirmation.jsx
+++ React-SnapShot/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // need useEffect because still execute after user cancel
+  // setTimeout(() => {
+  //   onConfirm();
+  // }, 3000);
+
+  useEffect(() => {
+    console.log('start timerout');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      console.log('clear timerout');
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
