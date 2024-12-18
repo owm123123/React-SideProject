b34f56d82508979d70d0d1bf1a6f814da5521ffb
owm30911 <owm30911@gmail.com>
2024-12-19 01:58:57 +0800

    add call api error handle & http.js

---
+++ React-CodeSendBox/http.js
@@ -0,0 +1,24 @@
+export async function fetchGetPlaces() {
+  const response = await fetch('http://localhost:3000/places');
+  if (!response.ok) {
+    throw new Error('Failed to fetch get places');
+  }
+  const data = await response.json();
+  return data.places;
+}
+
+export async function fetchUpdatePlaces(places) {
+  console.log(places);
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places: places }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch update places');
+  }
+  const data = await response.json();
+  return data.message;
+}
