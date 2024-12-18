42d3972b8b689d6f72961a1099359469925c54b2
owm30911 <owm30911@gmail.com>
2024-12-18 22:40:05 +0800

    learn fetch & useEffect call api

--- React-CodeSendBox/src/components/AvailablePlaces.jsx
+++ React-CodeSendBox/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setAvailablePlaces(data.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
