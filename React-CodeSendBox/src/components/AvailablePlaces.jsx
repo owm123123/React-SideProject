45f0eb5a8f6c877c4bcd63c8ba2120f0e9bcade5
owm30911 <owm30911@gmail.com>
2024-12-19 01:58:05 +0800

    add fetchUpdate function & use http.js manager api

--- React-CodeSendBox/src/components/AvailablePlaces.jsx
+++ React-CodeSendBox/src/components/AvailablePlaces.jsx
@@ -1,23 +1,47 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchGetPlaces } from '../../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function getPlaces() {
-      const response = await fetch('http://localhost:3000/places');
-      const places = response.json();
-      setAvailablePlaces(places);
+      setIsFetching(true);
+      try {
+        const places = await fetchGetPlaces();
+        setAvailablePlaces(places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({ message: error.message || 'Failed to call getPlaces api' });
+      }
+      setIsFetching(false);
     }
     getPlaces();
   }, []);
+
+  if (error) {
+    return <Error title={'Sorry... Fail Occurred'} message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
-      fallbackText="No places available."
+      isLoading={isFetching}
+      fallbackText="places is loading..."
       onSelectPlace={onSelectPlace}
     />
   );
