eaf15f7c5a4ca4e96d23308c87385492922fd429
long hao <owm30912@mitac.com.tw>
2024-12-23 21:30:33 +0800

    add availablePlaces sort

--- React-CustomHook/eslint.config.js
+++ React-CustomHook/eslint.config.js
@@ -19,6 +19,7 @@ export default [
       'prefer-const': 'warn',
       'no-constant-binary-expression': 'error',
       'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   },
 ];
--- React-CustomHook/src/hooks/useFetch.js
+++ React-CustomHook/src/hooks/useFetch.js
@@ -11,6 +11,7 @@ export function useFetch(callBackFn, init) {
       setIsFetching(true);
       try {
         const places = await callBackFn();
+        console.log(places);
         setFetchedData(places);
       } catch (error) {
         setError({
