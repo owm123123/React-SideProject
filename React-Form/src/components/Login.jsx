a783f4640cb8b175b43705b6c028db9c0f01f343
long hao <owm30912@mitac.com.tw>
2024-12-24 18:07:39 +0800

    learn useStatus collect input data

--- React-Form/src/components/Login.jsx
+++ React-Form/src/components/Login.jsx
@@ -1,17 +1,56 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredValue, setEnteredValue] = useState({
+    email: '',
+    password: '',
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(
+      `email: ${enteredValue.email}, password: ${enteredValue.password}`
+    );
+  }
+
+  function handleEnteredValue(id, value) {
+    setEnteredValue((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  }
+
   return (
-    <form>
+    // 在form的button中,default的type都是submit
+    // onSubmit: 當form裡面的button.type = submit時會觸發
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) =>
+              handleEnteredValue('email', event.target.value)
+            }
+            value={enteredValue.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleEnteredValue('password', event.target.value)
+            }
+            value={enteredValue.password}
+          />
         </div>
       </div>
 
