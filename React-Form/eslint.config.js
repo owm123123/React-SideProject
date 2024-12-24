35b1b9aac2fba7b1d07e8b97c0a097e33133dbe9
long hao <owm30912@mitac.com.tw>
2024-12-24 10:40:24 +0800

    init form project

---
+++ React-Form/eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'no-undef': 'error',
+      'no-unused-vars': 'warn',
+      'prefer-const': 'warn',
+      'no-constant-binary-expression': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+];
