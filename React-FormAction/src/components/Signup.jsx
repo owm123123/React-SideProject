f56c37ca8798f15849ba984b7f2092fa6cfb1680
long hao <owm30912@mitac.com.tw>
2024-12-26 18:39:07 +0800

    learn form redirect data

--- React-FormAction/src/components/Signup.jsx
+++ React-FormAction/src/components/Signup.jsx
@@ -6,48 +6,59 @@ import {
   hasMinLength,
 } from '../../../React-Form/src/util/validation';
 
-export default function Signup() {
-  function submitSignup(pervData, formData) {
-    const email = formData.get('email');
-    const password = formData.get('password');
-    const confirmPassword = formData.get('confirm-password');
-    const firstName = formData.get('first-name');
-    const lastName = formData.get('last-name');
-    const role = formData.get('role');
-    const acquisition = formData.getAll('acquisition');
-    const terms = formData.get('terms');
+export function submitSignup(pervData, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const confirmPassword = formData.get('confirm-password');
+  const firstName = formData.get('first-name');
+  const lastName = formData.get('last-name');
+  const role = formData.get('role');
+  const acquisition = formData.getAll('acquisition');
+  const terms = formData.get('terms');
 
-    const errors = [];
-    if (!isEmail(email) || !isNotEmpty(email)) {
-      errors.push('email error');
-    }
-    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
-      errors.push('password error');
-    }
-    if (!isEqualsToOtherValue(password, confirmPassword)) {
-      errors.push('password and confirmPassword not match');
-    }
-    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
-      errors.push('firstName or lastName error');
-    }
-    if (!isNotEmpty(role)) {
-      errors.push('role error');
-    }
-    console.log(`acquisition: ${acquisition}`);
-    if (acquisition.length === 0) {
-      errors.push('role error');
-    }
-    if (!terms) {
-      errors.push('terms error');
-    }
-
-    if (errors.length !== 0) {
-      return { errors };
-    }
-
-    return { errors: null };
+  const errors = [];
+  if (!isEmail(email) || !isNotEmpty(email)) {
+    errors.push('email error');
+  }
+  if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+    errors.push('password error');
+  }
+  if (!isEqualsToOtherValue(password, confirmPassword)) {
+    errors.push('password and confirmPassword not match');
+  }
+  if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+    errors.push('firstName or lastName error');
+  }
+  if (!isNotEmpty(role)) {
+    errors.push('role error');
+  }
+  if (acquisition.length === 0) {
+    errors.push('acquisition error');
+  }
+  if (!terms) {
+    errors.push('terms error');
   }
 
+  if (errors.length !== 0) {
+    return {
+      errors,
+      enteredValues: {
+        email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName,
+        role,
+        acquisition,
+        terms,
+      },
+    };
+  }
+
+  return { errors: null };
+}
+
+export default function Signup() {
   // const [state, formAction, isPending] = useActionState(fn, initialState, permalink?);
   // state: Fn return value
   // formAction: 調用formAction時,會再次觸發Fn
@@ -65,13 +76,23 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          defaultValue={formState.enteredValues?.email}
+        />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            defaultValue={formState.enteredValues?.password}
+          />
         </div>
 
         <div className="control">
@@ -80,6 +101,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -89,18 +111,33 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            name="first-name"
+            defaultValue={formState.enteredValues?.firstName}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            name="last-name"
+            defaultValue={formState.enteredValues?.lastName}
+          />
         </div>
       </div>
-
+      {/* {console.log(formState.enteredValues?.role)} */}
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        {/* TODO defaultValue not working */}
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role}
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -109,6 +146,7 @@ export default function Signup() {
         </select>
       </div>
 
+      {/* input checkBox 的 default 要用 "defaultChecked" */}
       <fieldset>
         <legend>How did you find us?</legend>
         <div className="control">
@@ -117,6 +155,9 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisition.includes(
+              'google'
+            )}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -127,20 +168,36 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisition.includes(
+              'friend'
+            )}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisition.includes(
+              'other'
+            )}
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultChecked={formState.enteredValues?.terms}
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
